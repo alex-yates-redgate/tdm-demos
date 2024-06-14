e77be8998362764aa2b153fae709653c791d3661
Alex Yates (Redgate) <140410709+alex-yates-redgate@users.noreply.github.com>
2024-06-14 16:42:51 +0100

    Update Subsetter_Filter.bat

--- simple-demos/Subsetter/Subsetter_Filter.bat
+++ simple-demos/Subsetter/Subsetter_Filter.bat
@@ -1,6 +1,8 @@
 subsetter.exe ^
 --database-engine sqlserver ^
---source-connection-string "Server=localhost\SQLEXPRESS;Database=AdventureWorks_Prod;trusted_connection=yes;TrustServerCertificate=yes" ^
---target-connection-string "Server=localhost\SQLEXPRESS;Database=AdventureWorks_Subset;trusted_connection=yes;TrustServerCertificate=yes" ^
+--source-connection-string "Server=Win2016;Database=AdventureWorks;trusted_connection=yes;TrustServerCertificate=yes" ^
+--target-connection-string "Server=Win2016;Database=AdventureWorks_TDM;trusted_connection=yes;TrustServerCertificate=yes" ^
 --target-database-write-mode Overwrite ^
 --config-file "./Filter.yaml"
+
+pause
