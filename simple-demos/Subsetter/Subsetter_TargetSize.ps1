4e6920a6ff4a0d0d7060337c461e0e05a985e843
Alex Yates (Redgate) <140410709+alex-yates-redgate@users.noreply.github.com>
2024-06-17 10:33:16 +0100

    Typo

--- simple-demos/Subsetter/Subsetter_TargetSize.ps1
+++ simple-demos/Subsetter/Subsetter_TargetSize.ps1
@@ -1,7 +1,7 @@
 Import-Module SqlServer
 
 $projRoot = "C:\git\tdm-demos\simple-demos"
-Write-Ouptut "Project root is: $projRoot"
+Write-Output "Project root is: $projRoot"
 
 Write-Host "Resetting AdventureWorks_TDM..."
 Invoke-Sqlcmd -InputFile "$projRoot\Database Reset\DBCC_Database_Clone-AdventureWorks.sql" -ServerInstance "WIN2016\"
