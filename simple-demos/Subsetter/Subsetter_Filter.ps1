315b34595250fb566e95d8806c45fc768c939324
Alex Yates (Redgate) <140410709+alex-yates-redgate@users.noreply.github.com>
2024-06-17 11:05:53 +0100

    Explicit paths

--- simple-demos/Subsetter/Subsetter_Filter.ps1
+++ simple-demos/Subsetter/Subsetter_Filter.ps1
@@ -1,11 +1,14 @@
 Import-Module SqlServer
 
+$projRoot = "C:\git\tdm-demos\simple-demos"
+Write-Output "Project root is: $projRoot"
+
 Write-Host "Resetting AdventureWorks_TDM"
-Invoke-Sqlcmd -InputFile ".\Database Reset\DBCC_Database_Clone-AdventureWorks.sql" -ServerInstance "WIN2016\"
+Invoke-Sqlcmd -InputFile "$projRoot\Database Reset\DBCC_Database_Clone-AdventureWorks.sql" -ServerInstance "WIN2016\"
 
 subsetter.exe `
 --database-engine sqlserver `
 --source-connection-string "Server=localhost;Database=AdventureWorks_Prod;trusted_connection=yes;TrustServerCertificate=yes" `
 --target-connection-string "Server=localhost;Database=AdventureWorks_TDM;trusted_connection=yes;TrustServerCertificate=yes" `
 --target-database-write-mode Overwrite `
---config-file="C:\git\tdm-demos\simple-demos\Subsetter\Filter.yaml"
+--config-file="$projRoot\simple-demos\Subsetter\Filter.yaml"
