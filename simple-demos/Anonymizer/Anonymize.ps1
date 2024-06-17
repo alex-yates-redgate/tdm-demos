a4213c5dff3de20cad58fb383b3618df775fdc52
Alex Yates (Redgate) <140410709+alex-yates-redgate@users.noreply.github.com>
2024-06-17 10:13:51 +0100

    Full paths for anon.ps1

--- simple-demos/Anonymizer/Anonymize.ps1
+++ simple-demos/Anonymizer/Anonymize.ps1
@@ -1,15 +1,17 @@
 $databaseType = 'SqlServer'
 $connectionString = 'Server=localhost;Database=AdventureWorks_TDM;trusted_connection=yes;TrustServerCertificate=yes;'
+$projRoot = "C:\git\tdm-demos\simple-demos\Anonymizer"
 
 Write-Host "End-to-End TDM CLI"
 Write-Host "Database Type is - $databaseType"
 Write-Host "Connection String is - $connectionString"
+Write-Host "Project Root is - $projRoot"
 
 Write-Host "Classifying..."
-Anonymize.exe classify --database-engine $databaseType --connection-string $connectionString --classification-file classification.json --output-all-columns
+Anonymize.exe classify --database-engine $databaseType --connection-string $connectionString --classification-file "$projRoot\classification.json" --output-all-columns
 
 Write-Host "Mapping..."
-Anonymize.exe map --classification-file classification.json --masking-file masking.json
+Anonymize.exe map --classification-file "$projRoot\classification.json" --masking-file "$projRoot\masking.json"
 
 Write-Host "Masking..."
-Anonymize.exe mask --database-engine $databaseType --connection-string $connectionString --masking-file masking.json --options-file .\Anonymizer\anonymizeconfig.json
+Anonymize.exe mask --database-engine $databaseType --connection-string $connectionString --masking-file "$projRoot\masking.json" --options-file "$projRoot\anonymizeconfig.json"
