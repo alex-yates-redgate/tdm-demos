dc0c857e95dbcb45ac09a6de8b649a1858cd6a16
Alex Yates (Redgate) <140410709+alex-yates-redgate@users.noreply.github.com>
2024-06-17 10:26:11 +0100

    typo

--- simple-demos/Subsetter/Subsetter_Filter.bat
+++ simple-demos/Subsetter/Subsetter_Filter.bat
@@ -3,6 +3,6 @@ subsetter.exe ^
 --source-connection-string "Server=Win2016;Database=AdventureWorks;trusted_connection=yes;TrustServerCertificate=yes" ^
 --target-connection-string "Server=Win2016;Database=AdventureWorks_TDM;trusted_connection=yes;TrustServerCertificate=yes" ^
 --target-database-write-mode Overwrite ^
---config-file ""C:\git\tdm-demos\simple-demos\Subsetter\Filter.yaml"
+--config-file "C:\git\tdm-demos\simple-demos\Subsetter\Filter.yaml"
 
 pause
