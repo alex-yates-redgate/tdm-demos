35a97132545c6f4ea3978701f3f04d9d7709dc5c
Alex Yates (Redgate) <140410709+alex-yates-redgate@users.noreply.github.com>
2024-06-17 10:20:06 +0100

    Adventureworks prod is not called prod

--- simple-demos/Subsetter/Subsetter.ps1
+++ simple-demos/Subsetter/Subsetter.ps1
@@ -1,6 +1,6 @@
 subsetter.exe `
 --database-engine sqlserver `
---source-connection-string "Server=localhost;Database=AdventureWorks_Prod;trusted_connection=yes;TrustServerCertificate=yes" `
+--source-connection-string "Server=localhost;Database=AdventureWorks;trusted_connection=yes;TrustServerCertificate=yes" `
 --target-connection-string "Server=localhost;Database=AdventureWorks_TDM;trusted_connection=yes;TrustServerCertificate=yes" `
 --target-database-write-mode Overwrite `
 --config-file="C:\git\tdm-demos\simple-demos\Subsetter\Filter.yaml"
